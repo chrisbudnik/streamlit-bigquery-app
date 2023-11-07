e1b8ce27f321d6a722de756722c79bff8dfdcc90
chrisb <chrism.budnik@gmail.com>
2023-11-08 00:02:12 +0100

    update: move default params to query constructor

--- app/query.py
+++ app/query.py
@@ -11,32 +11,17 @@ class QueryParam:
     value: Optional[Any] = None
      
 
-DEFAULT_PARAMS = [
-    QueryParam(name="country"),
-    QueryParam(name="last_order_date", type="INT64"),
-    QueryParam(name="calculated_segment"),
-    QueryParam(name="best_department"),
-    QueryParam(name="best_subclass"),
-    QueryParam(name="cross_buying"),
-    QueryParam(name="country"),
-]
-
 class QueryConstructor:
-    def __init__(self, default_params: List[DefaultParam]):
+    def __init__(self):
         """
         Initialize the ParameterizedQuery with a list of default parameters.
         """
         # Convert the list of DefaultParam into a dictionary for easy access
-        self.params = {param.name: param for param in default_params}
+        self.country = QueryParam(name="country"),
+        self.region = QueryParam(name="region"),
+        self.country = QueryParam(name="calculated_segment"),
+
     
-    def set_param(self, name: str, value: Any):
-        """
-        Set the value of a parameter by name.
-        """
-        if name in self.params:
-            self.params[name].value = value
-        else:
-            raise ValueError(f"Parameter {name} not found in the default parameters.")
     
     def construct_query(self, base_sql: str) -> Tuple[str, QueryJobConfig]:
         """
