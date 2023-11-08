cc6735120abeadb55c0063289340b0cf4edb9e40
chrisb <chrism.budnik@gmail.com>
2023-11-08 23:09:28 +0100

    create: query config with defaults

---
+++ app/query_framework/query_config.py
@@ -0,0 +1,20 @@
+class QueryConfig:
+    DEFAULT_PARAMS_TYPES = {
+        "country": "STRING",
+        "region": "STRING",
+        "segment": "STRING",
+        "product": "STRING",
+        "top_category": "STRING",
+        "only_promo": "STRING",
+        "top_subcategory": "STRING",
+        "regular_buyer": "STRING",
+        "extended_group": "BOOL",
+        "exclude_high_risk_customers": "BOOL",
+        "exclude_low_risk_customers": "BOOL",
+    }
+    BASE_QUERY = """
+        SELECT
+        *
+        FROM `data-to-insights.ecommerce.all_sessions_raw`
+        WHERE 1 = 1
+        """
