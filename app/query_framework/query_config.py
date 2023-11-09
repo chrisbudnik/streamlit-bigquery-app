a34b1f18fa3e375bed7bb3c967a7d8d4db927a3e
chrisb <chrism.budnik@gmail.com>
2023-11-09 21:12:24 +0100

    add: default param values to ignore

--- app/query_framework/query_config.py
+++ app/query_framework/query_config.py
@@ -2,6 +2,7 @@ class QueryConfig:
     DEFAULT_PARAMS_TYPES = {
         "country": "STRING",
         "region": "STRING",
+        "date": "DATE",
         "segment": "STRING",
         "product": "STRING",
         "top_category": "STRING",
@@ -12,5 +13,8 @@ class QueryConfig:
         "exclude_high_risk_customers": "BOOL",
         "exclude_low_risk_customers": "BOOL",
     }
-    BASE_SQL = "SELECT * FROM `data-to-insights.ecommerce.all_sessions_raw` \
-        \nWHERE"
+    DEFAULT_VALUES_TO_IGNORE = [
+        'not included', False, None, []
+        ]
+    
+    BASE_SQL = "SELECT * FROM `data-to-insights.ecommerce.all_sessions_raw` \nWHERE"
